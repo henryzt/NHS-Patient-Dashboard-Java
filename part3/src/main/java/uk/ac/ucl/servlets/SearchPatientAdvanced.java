0537c82cec3b4533fe87e436142bd2b1ca1a8bb1
Henry Zhang <henryzhang9802@gmail.com>
2019-03-20 17:26:53 +0000

    started advanced search

--- part3/src/main/java/uk/ac/ucl/servlets/SearchPatientAdvanced.java
+++ part3/src/main/java/uk/ac/ucl/servlets/SearchPatientAdvanced.java
@@ -1,33 +1,37 @@
 package uk.ac.ucl.servlets;
+
 import uk.ac.ucl.model.Model;
 import uk.ac.ucl.model.ModelFactory;
 import uk.ac.ucl.model.Patient;
 
-import java.io.*;
-
-import java.util.List;
-import javax.servlet.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 
-
-@WebServlet("/search.html")
-public class SearchPatient extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        request.setAttribute("get", true);
-        forward(request, response);
-    }
+@WebServlet("/advanced-search.html")
+public class SearchPatientAdvanced extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("get", false);
+
         Model model = ModelFactory.getModel();
         String para = request.getParameter("search_string");
-        //if no search term is passed, terminate search
-        if(para.equals("")){
-            doGet(request,response);
-            return;
-        }
+        String ageMin = request.getParameter("min");
+        String ageMax = request.getParameter("max");
+        String gender = request.getParameter("gender");
+        String city = request.getParameter("city");
+        String initial = request.getParameter("initial");
+
+        List<Patient> patients = model.getPatients();
+
+
+
         List<Patient> searchResult = model.search(para);
         request.setAttribute("search_para", para);
         request.setAttribute("list", searchResult);
